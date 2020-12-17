bb4eb873460e37502da9c51c8c006645bcdd1c1a
thanhtamtotaa <thanhtamtotaa@gmail.com>
2020-12-17 10:45:28 +0700

    Update TotaaXa.php

--- src/Models/TotaaXa.php
+++ src/Models/TotaaXa.php
@@ -2,6 +2,7 @@
 
 namespace Totaa\TotaaDonvi\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Wildside\Userstamps\Userstamps;
